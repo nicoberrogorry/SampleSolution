69fcacbb4b25b2db3f37432d1bfafbff6a884988
Nico Berrogorry <nicoberrogorry@gmail.com>
2018-06-13 17:39:36 -0300

    Se agrega Persons a la base de datos.

---
+++ FirstSamplesolution/DataBase/FirstSampleSolutionDataBase/dbo/Tables/Persons.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Persons] (
+    [PersonId]      INT        IDENTITY (1, 1) NOT NULL,
+    [Name]          NCHAR (10) NOT NULL,
+    [LastName]      NCHAR (10) NOT NULL,
+    [Address]       NCHAR (10) NOT NULL,
+    [CellularPhone] NCHAR (10) NOT NULL
+);
+
