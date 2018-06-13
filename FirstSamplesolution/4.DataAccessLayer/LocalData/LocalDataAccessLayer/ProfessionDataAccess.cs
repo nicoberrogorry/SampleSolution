b912bfad2d0f86fdaafd986e68b691a362c29b1c
laptop-crivero <crivero1510@gmail.com>
2018-06-13 17:43:40 -0300

    nueva entidad

---
+++ FirstSamplesolution/4.DataAccessLayer/LocalData/LocalDataAccessLayer/ProfessionDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using BusinessEntities;
+using LocalDataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDataAccessLayer
+{
+    public class ProfessionDataAccess
+    {
+        public void CreateProfession(Profession p)
+        {
+            using (var dataContext = new ProfessionsDataContext())
+            {
+                dataContext.CreateProfession(p.Description);
+            }
+
+        }
+
+    }
+}
---
+++ FirstSamplesolution/DataBase/FirstSampleSolutionDataBase/dbo/Stored Procedures/CreateProfession.sql
@@ -0,0 +1,12 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE dbo.CreateProfession
+	 @description varchar
+AS
+BEGIN
+	--insert new profession
+	return 1
+END
