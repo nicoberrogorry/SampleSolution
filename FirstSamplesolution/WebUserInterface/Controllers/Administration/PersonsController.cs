faaec5095cf31b3a45f6bf0dadcf00b5800efeb4
laptop-crivero <crivero1510@gmail.com>
2018-06-14 17:28:47 -0300

    Dorpdownlist de oficios

--- FirstSamplesolution/WebUserInterface/Controllers/Administration/PersonsController.cs
+++ FirstSamplesolution/WebUserInterface/Controllers/Administration/PersonsController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using BusinessEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUserInterface.Models;
 
 namespace WebUserInterface.Controllers.Administration
 {
@@ -11,12 +13,34 @@ namespace WebUserInterface.Controllers.Administration
         // GET: Person
         public ActionResult Index()
         {
-            return View("create");
+            PersonViewModel model = new PersonViewModel();
+            model.Professions = this.GetProfessions();
+
+            return View("create", model);
         }
 
+        [HttpPost]
+        public ActionResult Create(PersonViewModel model)
+        {
+             model = new PersonViewModel();
+            model.Professions = this.GetProfessions();
+
+            return View("create", model);
+        }
         public ActionResult Search()
         {
             return View("search");
+        }
+
+        public List<Profession> GetProfessions()
+        {
+            List<Profession> result = new List<Profession>()
+            {
+                 new Profession (){ Description="Profession A", ProfessionId=1 },
+                 new Profession (){ Description="Profession B", ProfessionId=2 }
+            };
+
+            return result;
         }
     }
 }
---
+++ FirstSamplesolution/WebUserInterface/Models/PersonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUserInterface.Models
+{
+    public class PersonViewModel
+    {
+        public List<Profession> Professions { get; set; }
+
+        public int SelectedProfession { get; set; }
+    }
+
+}
