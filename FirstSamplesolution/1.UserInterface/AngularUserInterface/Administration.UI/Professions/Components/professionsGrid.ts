52da0b8161fd5123da2d49a8f7b68b862ad7cab6
laptop-crivero <crivero1510@gmail.com>
2018-07-02 19:48:38 -0300

    changes

---
+++ FirstSamplesolution/1.UserInterface/AngularUserInterface/Administration.UI/Professions/Components/professionsGrid.ts
@@ -0,0 +1,18 @@
+import {
+	Component,
+	ViewChild,
+	EventEmitter,
+	Input,
+	Output
+} from '@angular/core';
+@Component({
+	selector: 'professions-Grid',
+	templateUrl: './professionsGrid.html',
+	styleUrls: ['./professionsGrid.scss']
+})
+export class ProfessionsGrid{
+	constructor() {
+		
+	}
+
+}
