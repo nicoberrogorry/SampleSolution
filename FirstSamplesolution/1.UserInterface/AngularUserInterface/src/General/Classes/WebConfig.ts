d7a3ddafc0974839c3bbe29269b49bac97741088
Nico Berrogorry <nicoberrogorry@gmail.com>
2018-07-16 10:29:29 -0300

    Cambios del lado del servidor

--- FirstSamplesolution/1.UserInterface/AngularUserInterface/src/General/Classes/WebConfig.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-export class WebConfig
-{
-  public AdministrationServicesBaseUrl: string = "http://192.168.1.43/SampleSolutionServices/Administration/";
-  public PersonsServiceRelativeUrl: string =  "PersonsService.svc/REST/";
-  public ProfessionsServiceRelativeUrl: string = "ProfessionsService.svc/REST/";
-}
